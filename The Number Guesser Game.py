b89a712a107e69f82bccb8f0f4dc6ba7657c4f6c
Shourya Shukla <52319705+shourya-shukla@users.noreply.github.com>
2022-03-07 18:09:38 +0530

    Update The Number Guesser Game.py
    Let's see if you can guess the number,
    Now available with a leaderboard.

--- The Number Guesser Game.py
+++ The Number Guesser Game.py
@@ -1,19 +1,47 @@
-from random import randrange
-n = randrange(1, 100) # You can enter the range of your choice here
-g = 10 #The number of guesses
-print("Welcome to the number guesser game, Created by Shourya")
-while(g>0):
-   inp = int(input("Guess a number\n"))
-   if inp>n:
-      print("The number is smaller than "+str(inp))
-      g = g-1
-      print("No. of guesses left = "+str(g))
-   elif inp<n:
-      print("The number is larger than "+str(inp))
-      g = g-1
-      print("No. of guesses left = "+str(g))
-   else:
-      print("Congrats! You Won the Game")
-      break
+# Created for fun by: Shourya Shukla
+# For more such projects, please do visit https://github.com/shourya-shukla
+
+from random import randrange  # Module to get a random number which is to be guessed
+
+name = input("Please enter your name: ")
+print(f"Hello \"{name}\", Welcome to the number guessing game, brought to you by Shourya Shukla.\n")  # Welcome message
+d = int(input('''Select Difficulty:
+1-Easy
+2-Medium
+3-Hard
+==> '''))  # Select difficulty
+if d == 1:  # Easy mode
+    num = randrange(1, 10)
+    mode = "Easy"
+elif d == 2:  # Medium mode
+    num = randrange(1, 100)
+    mode = "Medium"
+elif d == 3:  # Hard mode
+    num = randrange(1, 1000)
+    mode = "Hard"
+else:  # Error
+    print("Invalid input")
+    exit()
+tries = 1
+unum = int(input("Okay!, let's start, guess a number: "))
+while unum != num:
+    if unum > num:  # If entered a larger number
+        unum = int(input("Please input a smaller number: "))
+    else:  # If entered a smaller number
+        unum = int(input("Please input a larger number: "))
+    tries = tries + 1
 else:
-   print("Game Over, Better Luck Next Time")
+    print("\nCongrats ! You've won the game.")  # Winning message
+    print(f"No. of tries: {tries}")  # Tries counter
+f = open("leaderboard.txt", "a")
+f.write(name+"|"+str(tries)+"|"+mode+"\n")  # Writing scores for leaderboard
+f.close()
+prompt = input("Do you want to see the leaderboard ?\n==> ")
+if prompt == "Yes":
+    f = open("leaderboard.txt")
+    print(f.read())  # Show leaderboard
+    f.close()
+elif prompt == "No":
+    exit()
+else:
+    print("Please enter Yes or No.")
